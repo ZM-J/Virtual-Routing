a4e6132e78ab03f55abe2be892e17a14c63734bc
MarshallW906 <MaticWang@hotmail.com>
2017-12-28 18:09:15 +0800

    init function body

--- src/RouteLS.cpp
+++ src/RouteLS.cpp
@@ -1,1 +1,24 @@
-#include "RouteLS.h"
+#include "RouteLS.hpp"
+
+#include <string>
+
+using namespace std;
+
+int RouteLS::SendRouteMsg(string msg, string dest) {
+    int errno = 0;
+    return errno;
+}
+
+int RouteLS::RecvRouteMsg(string msgGet) {
+    int errno = 0;
+    return errno;
+}
+
+string RouteLS::serializeMsg(std::vector<std::vector<int> >& table) {
+    string ret;
+    return ret;
+}
+std::vector<std::vector<int> > RouteLS::parseMsg(string msg) {
+    std::vector<std::vector<int> > ret;
+    return ret;
+}
