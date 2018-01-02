7c3877d3fd7c5931830f59fdad08ea91a13ddf01
MarshallW906 <MaticWang@hotmail.com>
2018-01-02 22:23:52 +0800

    modify func definitions

--- include/route_algo.h
+++ include/route_algo.h
@@ -1,24 +1,25 @@
 // base class for different routing algorithm
 
 #include "./args.h"
-#include "./route_message.h"
-#include "./socket_sender.h"
-#include "./thread_safe_queue.h"
 
 #include <map>
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class RouteAlgo {
    public:
-    virtual int SendRouteMsg(std::string, std::string) = 0;
-    virtual int RecvRouteMsg(std::string&) = 0;
-
-    int StartRecv();
+    virtual int UpdateRouteMsg(const std::string&) = 0;
+    virtual vector<pair<string, string>> GenerateRouteMsg() = 0;
     virtual std::vector<int> JudgePath(int, int) = 0;
+
+    // Receive a string message which is a Reachability raw Message
+    int UpdateConnectivity(const std::string&);
 
    protected:
     RouteAlgo() = default;
     ~RouteAlgo() = default;
     std::vector<std::vector<int>> route_table_;
+    std::vector<std::vector<int>> connectivity_table_;
 };
--- src/route_algo.cpp
+++ src/route_algo.cpp
@@ -1,1 +1,14 @@
 #include "route_algo.h"
+#include "route_message.h"
+
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int RouteAlgo::UpdateConnectivity(const std::string& raw_msg_str) {
+    ReachabilityMessage r_msg(raw_msg_str);
+    // update the connectivity_table_
+    // ..
+    return 0;
+}
