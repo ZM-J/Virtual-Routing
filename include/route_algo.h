2d551d12768c734af3e91de8d186a3b95d3819be
MarshallW906 <MaticWang@hotmail.com>
2018-01-02 20:58:39 +0800

    move sender & receiver to RouteNode

--- include/route_algo.h
+++ include/route_algo.h
@@ -1,6 +1,9 @@
 // base class for different routing algorithm
 
 #include "./args.h"
+#include "./route_message.h"
+#include "./socket_sender.h"
+#include "./thread_safe_queue.h"
 
 #include <map>
 #include <string>
@@ -10,11 +13,12 @@ class RouteAlgo {
    public:
     virtual int SendRouteMsg(std::string, std::string) = 0;
     virtual int RecvRouteMsg(std::string&) = 0;
+
+    int StartRecv();
     virtual std::vector<int> JudgePath(int, int) = 0;
 
    protected:
+    RouteAlgo() = default;
+    ~RouteAlgo() = default;
     std::vector<std::vector<int>> route_table_;
-    std::map<std::string, ReachabilityEnum> connectivity_;
-    int SendConnectionCheck();
-    int RecvConnectionCheck();
 };
--- src/route_algo.cpp
+++ src/route_algo.cpp
@@ -1,14 +1,1 @@
 #include "route_algo.h"
-#include "route_message.h"
-
-int RouteAlgo::SendConnectionCheck() {
-    // Check Connection
-    // Can use same port as SendRouteMsg()?
-    return 0;
-}
-
-int RouteAlgo::RecvConnectionCheck() {
-    // Receive Connection
-    // Can use same port as RecvRouteMsg()?
-    return 0;
-}
